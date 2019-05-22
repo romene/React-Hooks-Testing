7a4a8b09486ca53b212a547f9a834250dbf6573d
romene <rogerio.eire@gmail.com>
2019-05-23 10:14:46 +1200

    simple counter with react Hooks

--- src/Hooks.js
+++ src/Hooks.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function Hooks() {
+function Hooks(props) {
+
+  //call useState [value, function to set the value] = the initial value  
+  const [count, setCount] = useState(props.initialValue)
+
+
+  console.log(props)
+  console.log(count)
   return (
     <div className="App">
-      <h1>The Current count is 0</h1>
+      <h1>The Current count is {count} </h1>
+      <button onClick={() => setCount(count + 1)}>Add 1</button>
+      <button onClick={() => setCount(props.initialValue)}>Reset Counter</button>
+      <button onClick={() => setCount(count - 1)}>Sub 1</button>
     </div>
   );
 }
+
+Hooks.defaultProps = {
+  initialValue: 0
+}
+
 
 export default Hooks;
