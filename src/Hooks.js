7f87cdef1d279bfa056982dc19b9a99a1b5360dd
romene <rogerio.eire@gmail.com>
2019-05-23 09:57:36 +1200

    cleaning up react-create-app

---
+++ src/Hooks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Hooks() {
+  return (
+    <div className="App">
+      <h1>The Current count is 0</h1>
+    </div>
+  );
+}
+
+export default Hooks;
